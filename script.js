7ee3289d74be506e12d405bd268f4b09f361cb20
Ayla Kulifaj <aylakulifaj@gmail.com>
2024-04-01 19:07:39 -0500

    Created an attack Function, Updated State Variables, and a condition so a value cant be less than 0

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+let myAP = 100;
+let fungusHP = 100;
 
-function onReady() {
+
+function onReady(event) {
     console.log("Ready to go!")
+//6.
+const arcane_scepter = document.getElementByClassName('attack-btn arcane-scepter');
+const entangle = document.getElementByClassName('attack-btn entangle');
+const dragon_blade= document.getElementByClassName('attack-btn dragon-blade');
+const star_fire = document.getElementByClassName('attack-btn star-fire');
+
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
@@ -18,3 +26,19 @@ function onReady() {
 
 
 onReady()
+//2.creating a function that will update the variables myAp & myHP
+//3.creating paramters cost & damage
+function attack(cost,damage){
+myAP-=cost
+fungusHP-=damage
+if (myAP<=0) {
+    myAP=0
+    console.log("You Die!! :( Your points now are:  ", `${myAP}`)
+} //5.Making sure it never goes into negative AP or HP
+if (fungusHP<=0) {
+    fungusHP=0
+    console.log("You Die!! :( Your point now are: ",  `${fungusHP}`)
+}
+console.log(myAP);
+console.log(fungusHP);
+}
